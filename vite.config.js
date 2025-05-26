188d5e0eb4901c496a23fc7670d4a41e0cf29513
Clyde <morallosclydeofficialpersonal@gmail.com>
2025-05-26 14:47:29 +0800

    vite config edit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,20 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/app.css", 
+                // "resources/js/app.js", 
+                'resources/js/registration/student-store.js',
+                'resources/js/registration/download-qr.js',
+                'resources/js/student/delete-student.js',
+                'resources/js/scan/scan.js',
+                'resources/js/scan/scan2.js',
+                'resources/js/scan/phonescan.js',
+                // 'resources/js/delete-attendances.js',
+                'resources/js/auth/login.js',
+                'resources/js/instructor/delete.js',
+                'resources/js/instructor/store.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
